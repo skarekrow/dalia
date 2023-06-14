fb56fdf767f2b44232296439fc1c9fb66c0adbe3
Brandon Schneider <bschneid@redhat.com>
2023-06-14 13:35:41 -0500

    Capitialize
    Signed-off-by: Brandon Schneider <bschneid@redhat.com>

--- api/src/main.rs
+++ api/src/main.rs
@@ -33,14 +33,14 @@ async fn current_weather() -> impl Responder {
     let mut weather: CurrentWeather = response.unwrap().json().await.unwrap();
 
     if weather.location.tz_id.contains("America/") {
-        weather.current.temp = Some(format!("{}f", weather.current.temp_f));
+        weather.current.temp = Some(format!("{}F", weather.current.temp_f));
         weather.current.wind_speed = Some(format!("{}mph", weather.current.wind_mph));
-        weather.current.feels_like = Some(format!("{}f", weather.current.feelslike_f));
+        weather.current.feels_like = Some(format!("{}F", weather.current.feelslike_f));
         weather.current.gust = Some(format!("{}mph", weather.current.gust_mph));
     } else {
-        weather.current.temp = Some(format!("{}c", weather.current.temp_c));
+        weather.current.temp = Some(format!("{}C", weather.current.temp_c));
         weather.current.wind_speed = Some(format!("{}kph", weather.current.wind_kph));
-        weather.current.feels_like = Some(format!("{}c", weather.current.feelslike_c));
+        weather.current.feels_like = Some(format!("{}C", weather.current.feelslike_c));
         weather.current.gust = Some(format!("{}kph", weather.current.gust_kph));
     }
 
